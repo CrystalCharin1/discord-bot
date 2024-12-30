016ebbe753f16de6c952b17dfdcee5963ea45cef
CrystalCharin <65487320+CrystalCharin1@users.noreply.github.com>
2024-12-30 10:35:51 +0300

    Delete discord_bot/discord_bot directory

--- discord_bot/discord_bot/utils/config.py
+++
@@ -1,6 +0,0 @@
-# Ключевые слова для анализа текста
-KEYWORDS = ["мат", "оскорбление", "запрещенное слово"]
-
-# Пути к данным
-AUDIO_FOLDER = "data/user_audio"
-MODEL_PATH = "data/models/voice_recognition_model.h5"
